79b197dbb956e576865fdd2f575df6a16f6ed53f
Dharmin <dharminlive12@live.com>
2019-10-02 14:44:25 -0500

    a

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ const fnerr = document.getElementById('helper-text');
 
 fname.addEventListener('onblur', validateFirstName,)
 
+if (validateFirstName == false) {
+  document.getElementById("register").innerHTML = "black";
+}
+
 function validateFirstName(event) {
   // console.log(fname.value)
-  if (fname.value === '') {
-    fnerr.innerHTML = 'hello'
-  }
+  if (fname.value === 'black') {
+    // fnerr.innerHTML = 'hello'
+    return false
+  } else
+  return true;
 }
