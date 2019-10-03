f964b502e4ab402b00087b3d596234a9be4c9658
Dharmin <dharminlive12@live.com>
2019-10-03 15:02:25 -0500

    v.0.5
    Next phase of JS in place.

--- script.js
+++ script.js
@@ -4,21 +4,41 @@ const lname = document.querySelector('#lastname');
 const email = document.querySelector('#email');
 const pass = document.querySelector('#password');
 
+
 console.log(mainForm)
 
-// fname.value
 
 
-fname.addEventListener("input", function(event) {
-  if(!fname.value == ""){
-    fname.nextElementSibling
-  }
-}
 
-mainForm.addEventListener("submit", function(event) {
-  if (fname.value == "" || fname.value == null) {
-    event.preventDefault();
-    fname.nextElementSibling.innerHTML = 'hi'
-  }
+
+// mainForm.addEventListener("submit", function(event) {
+//   if (fname.value == "" || fname.value == null) {
+//     event.preventDefault();
+//     var fmessage = document.createTextNode("Please put something!");
+//     fname.nextElementSibling.appendChild(fmessage);
+//   }
+// });
+
+fname.addEventListener("focus", function(event) {
+  console.log('hello')
+const ferror = document.createElement("span")
+const message = document.createTextNode("swag");
+ferror.appendChild(message);
+fname.nextElementSibling.appendChild(ferror);
 
 })
+
+lname.addEventListener("focus", function(event){
+  errorbase = fname.nextElementSibling
+  if (errorbase.hasChildNodes()) {
+    errorbase.removeChild(errorbase.childNodes[0]);
+  }
+
+  // console.log(fname.nextElementSibling)
+
+
+});
+
+// fname.nextElementSibling.removeChild();
+// var clear = document.createTextNode("");
+// fname.nextElementSibling.appendChild(clear);
