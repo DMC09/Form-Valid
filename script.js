271cf8b3cb4525fa76360e694d3d11f5e9666aaf
Dharmin <dharminlive12@live.com>
2019-10-07 14:05:54 -0500

    v.0.6
    Restructed JS portion to validate only through JS.

--- script.js
+++ script.js
@@ -1,122 +1,174 @@
-// Add the novalidate attribute when the JS loads
-var forms = document.querySelectorAll('.validate');
-for (var i = 0; i < forms.length; i++) {
-  forms[i].setAttribute('novalidate', true);
+//input field
+const firstName = document.getElementById("firstName")
+const lastName = document.getElementById("lastName")
+const password = document.getElementById("password")
+const confirmPassword = document.getElementById("confirmPassword")
+const email = document.getElementById("email")
+
+//form
+const form = document.getElementById("myForm")
+
+//validation colors
+const green = "#4caf50"
+const red = "#f44336"
+
+
+//handle form
+form.addEventListener('submit', function(event) {
+  // Prevent default behaviour
+  if (
+    !validateFirstName() &&
+    !validateLastName() &&
+    !validatePassword() &&
+    // !validateConfirmPassword() ||
+    !validateEmail()
+  ) event.preventDefault();
+   console.log('hello');
+});
+
+//validating functions
+function validateFirstName() {
+  // check if is empty
+  if (checkIfEmpty(firstName)) return;
+  //check if it has letters
+  if (checkIfOnlyLetters(firstName)) return;
+  return true;
 }
 
-// Validates the field
-var hasError = function(field) {
-  //gets the validity
-  var validity = field.validity;
-  // retuns if field is valid
-  if (validity.valid) return;
-  // retuns if field is empty
-  if (validity.valueMissing) return 'Please fill out this field';
-  // retuns if field is short
-  if (validity.tooShort) return 'Please lengthen this text.';
-  // retuns if field is long
-  if (validity.tooLong) return 'Please shorten this text.';
-  // retuns if field is not the right pattern
-  if (validity.patternMismatch) {
-    if (field.type === 'email') return 'Please include these characters 1 uppercase 1 lowercase 1 number';
-    if (field.type === 'password') return 'hello';
-  }
-  //generic message
-  return 'we cannot accept this'
+function validateLastName() {
+  // check if is empty
+  if (checkIfEmpty(lastName)) return;
+  //check if it has letters
+  if (checkIfOnlyLetters(lastName)) return;
+  return true;
 }
 
-//function to show the erros
-var showError = function(field, error) {
-
-  //Add a class
-  field.classList.add('error');
-
-  //gets the field id or name
-  var id = field.id || field.name;
-  if (!id) return;
-
-  //checks if the message is already present
-  //if not, create one
-  var message = field.form.querySelector('.error-massage#error-for-' + id)
-  if (!message) {
-    message = document.createElement('div');
-    message.className = 'error-message';
-    message.id = 'error-for-' + id;
-
-    //case when there is no label
-    // if (!label) {
-    //   field.parentNode.insertBefore(message, field.nextSibling)
-    // }
-  }
-
-  //add ARIA role to the field
-  field.setAttribute('aria-describedby', 'error-for-' + id);
-
-  //update error message
-  message.innerHTML = error;
-
-  //show error message
-  message.style.display = 'block';
-  message.style.visibility = 'visible';
-
-};
-
-
-var removeError = function (field ) {
-
-  //remove Erro class to field.
-  field.classList.remove('error');
-
-  //removes Aria
-  field.removeAttribute('aria-describedby');
-
-  // get the field id
-  var id = field.id || field.name;
-  if (!id) return;
-
-  // check is the error is in the dom
-  var message = field.form.querySelector('.error-message#error-for-' + id + "")
-
-  //if there is an error hide it
-  message.innerHTML = ''
-  message.style.display = 'none'
-  message.style.visibility = 'hidden'
+function validatePassword(){
+  //empty
+  if(checkIfEmpty(password)) return;
+  //length requirements
+  if(!meetLength(password,4,20)) return;
+  //check password regex
+  //1-a
+  //2 -a 1
+  //3- A a 1
+  //4- A a 1 @
+  if(!containsCharacters(password, 1)) return;
+  return true
 }
 
-//listens to all blue events
-document.addEventListener('blur', function (event) {
-
-    // Only run if the field is in a form to be validated
-    if (!event.target.form.classList.contains('validate')) return;
-
-    // Validate the field
-    var error = hasError(event.target);
-
-    // If there's an error, show it
-    if (error) {
-        showError(event.target, error);
-        return;
-    }
-
-    // Otherwise, remove any existing error message
-    removeError(event.target);
-
-}, true);
-
-//check all fiels on submit
-document.addEventListener('submit', function (event){
-
-  //only run on forms with validate
-  if (!event.target.classList.contains('validate')) return;
-
-  //get all of the form elements
-  var fields = event.target.elements
-
-  //validate field
-  var error, hasErrors
-
-  //if there is an error do not usbmit the form
-  if(hasErrors) {
-    event.preventDefault;
+function validateConfirmPassword(){
+  if(password.className !== "valid"){
+    setInvalid(confirmPassword, `Password must be valid`);
+    return;
   }
-}, false)
+  // check if they are equal
+  if(password.value !== confirmPassword.value){
+    setInvalid(confirmPassword,`Passwords must match`)
+    return;
+  } else {
+    setValid(confirmPassword)
+  }
+  return true;
+}
+
+function validateEmail(){
+  if(checkIfEmpty(email)) return;
+  if(!containsCharacters(email, 5)) return;
+  return true;
+}
+
+//utility functions
+//process to handle if field is potentially empty
+function checkIfEmpty(field) {
+  if (isEmpty(field.value.trim())) {
+    //set valid as invalid
+    setInvalid(field , `${field.name} must not be empty`);
+    return true;
+  } else {
+    //set field valid
+    setValid(field);
+    return false;
+  }
+}
+
+//fucntion to check if field is empty
+function isEmpty(value) {
+  if (value === '') return true;
+  return false;
+}
+
+
+//invalid set
+function setInvalid(field, message) {
+  field.className = 'invalid';
+  field.nextElementSibling.innerHTML = message;
+  field.nextElementSibling.style.color = red;
+}
+//valid set
+function setValid(field) {
+  field.className = 'valid';
+  field.nextElementSibling.innerHTML = '';
+  // field.nextElementSibling.style.color = green;
+}
+
+
+function checkIfOnlyLetters(field){
+  if(/^[a-zA-Z ]+$/.test(field.value)) {
+    setValid(field);
+    return true;
+  } else {
+    setInvalid(field,`${field.name} must only contain letters`)
+  } return false;
+}
+
+function meetLength(field,minLength,maxLength){
+  if(field.value.length >= minLength && field.value.length < maxLength) {
+    setValid(field);
+    return true;
+  } else if (field.value.length < minLength){
+    setInvalid(field, `${field.name} must be at least ${minLength} characters `)
+    return false
+  } else {
+    setInvalid(field, `${field.name} must be shorter than ${maxLength} characters `)
+    return false;
+  }
+}
+
+function containsCharacters (field,code) {
+  let regEx;
+  switch(code){
+    case 1:
+    //letters
+    regEx = /(?=.*[a-zA-Z])/
+    return matchWithRegEx(regEx, field,`must contain at least 1 letter`)
+    case 2:
+    //letter and number
+    regEx = /(?=.*\d)(?=.*[a-zA-Z])/
+    return matchWithRegEx(regEx, field,`must contain at least 1 letter and number`)
+    case 3:
+    //letter and number
+    regEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/
+    return matchWithRegEx(regEx, field,`must contain these characters:1 Upper case 1 Lower case 1 Number`)
+    case 4:
+    //letter and number
+    regEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/
+    return matchWithRegEx(regEx, field,`must contain these characters:1 Upper case 1 Lower case 1 Number, 1 special symbol`)
+    case 5:
+    //email
+    regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return matchWithRegEx(regEx, field,`Must be a valid email address`)
+    default:
+      return false;
+  }
+}
+
+function matchWithRegEx (regEx, field, message) {
+  if(field.value.match(regEx)){
+    setValid(field);
+    return true;
+  } else {
+    setInvalid(field,message);
+    return false;
+  }
+}
